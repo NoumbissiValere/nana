7a694a88170fead364720819cf234e022c8dd034
Perside Rosalie <persiderosalie@gmail.com>
2016-09-22 20:30:36 +0100

    added a landing page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 /**
- * AngularJS Tutorial 1
- * @author Nick Kaye <nick.c.kaye@gmail.com>
- */
-
-/**
- * Main AngularJS Web Application
+ * 
  */
 var app = angular.module('tutorialWebApp', [
   'ngRoute'
