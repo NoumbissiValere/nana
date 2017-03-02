6cd4c1e8846d396bd4227735265c8c32878ce4fb
rosalieper <persiderosalie@gmail.com>
2017-03-02 11:32:20 +0100

    adding phpmailer lib

---
+++ PHP_Scripts/sendmail.php
@@ -0,0 +1,84 @@
+<?php
+	if (isset($_POST["submit"])) {
+		$name = $_POST['contact_name'];
+		$email = $_POST['contact_email'];
+		$message = $_POST['contact_message'];
+		$from = $email; 
+		$to = 'persiderosalie@gmail.com'; 
+		$subject = 'Message from Contact ABEBOH';
+		
+		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+ 
+		// Check if name has been entered
+		if (!$_POST['contact_name']) {
+			$errName = 'Please enter your name';
+		}
+		
+		// Check for email
+		if (!$_POST['contact_email'] || !filter_var($_POST['contact_email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+		
+		//Check for message
+		if (!$_POST['contact_message']) {
+			$errMessage = 'Please enter your message';
+		}
+		
+ 
+	// send the email
+
+		require 'PHPMailer/PHPMailerAutoload.php';
+
+		//PHPMailer Object
+		$mail = new PHPMailer;
+		
+		$mail->isSMTP();
+		//Enable SMTP debugging
+		// 0 = off (for production use)
+		// 1 = client messages
+		// 2 = client and server messages
+		$mail->SMTPDebug = 2;
+		//Ask for HTML-friendly debug output
+		$mail->Debugoutput = 'html';
+		//Set the hostname of the mail server
+		$mail->Host = "smtp.gmail.com";
+		//Set the SMTP port number - likely to be 25, 465 or 587
+		$mail->Port = 587;
+		
+		//enable smtp authentication
+		$mail->SMTPAuth = true;
+		
+		//Provide username and password     
+		$mail->Username = "rosalieperside@gmail.com";
+		$mail->Password = "perper2014"; 
+
+		//If SMTP requires TLS encryption then set it
+		$mail->SMTPSecure = "tls"; 
+		
+		//From email address and name
+		$mail->From = $email;
+		$mail->FromName = $name;
+
+		//To address and name
+		$mail->addAddress($to);//recipient address
+
+		//Address to which recipient will reply
+		$mail->addReplyTo($email, "Reply");
+
+		//Send HTML or Plain Text email
+		$mail->isHTML(true);
+
+		$mail->Subject = $subject;
+		$mail->Body = $body;//html version
+		//$mail->AltBody = "This is the plain text version of the email content";
+
+		if(!$mail->send()) 
+		{
+				echo "Mailer Error: " . $mail->ErrorInfo;
+		} 
+		else 
+		{
+				echo "Message has been sent successfully";
+		}
+}
+?>
--- partials/contact.php
+++ partials/contact.php
@@ -87,6 +87,7 @@
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1989.648382904865!2d9.297514975833893!3d4.161946674377226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNMKwMDknNDMuMCJOIDnCsDE3JzU1LjAiRQ!5e0!3m2!1sen!2scm!4v1474547888146" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
 <hr>
+<?php require_once '../PHP_Scripts/sendemail.php';?>
 </div><!-- /.container -->
 <br>
 
